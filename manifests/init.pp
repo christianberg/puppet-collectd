878d6761c898a9826e47e91ab49c5e30ddfebcc1
Christian Berg <berg.christian@gmail.com>
2012-12-13 17:15:20 +0100

    Include collectd.conf with graphite integration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,24 @@
-class collectd {
+class collectd ($graphite_server, $graphite_port = 2003) {
   case $::operatingsystem {
     debian, ubuntu: {
       include apt
 
       apt::ppa { 'ppa:joey-imbasciano/collectd5': }
       package { 'collectd':
-        require => Apt::Ppa['ppa:joey-imbasciano/collectd5']
+        require => Apt::Ppa['ppa:joey-imbasciano/collectd5'],
+        ensure  => latest,
+        before  => File['/etc/collectd.conf'],
+      }
+
+      file { '/etc/collectd.conf':
+        ensure  => present,
+        content => template('collectd/collectd.conf.erb'),
+      }
+
+      service { 'collectd':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['/etc/collectd.conf'],
       }
     }
     default: { notice "Unsupported operatingsystem ${::operatingsystem}" }
